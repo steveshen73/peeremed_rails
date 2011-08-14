116687084fbfbda16fcd02c0c41b8752b8c911b2
Steve Shen <steveshen73@gmail.com>
2011-08-14 09:32:14 -0700

    adding my first rails scripts, ya, ya, ya

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '03d224c2ad73a42bd7651b8436b840e3d26ea1621da7345d39274d6e988404f1a6cb3c5de5a8cd11e2ad6dbbd7acb3a367a977996178d40e953ce6560bcbf2f8'
---
+++ testapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '8d8138e9201a5d18808b5f69cce97eaa2fa3b08c6697ebb76ae48961c1e79042de2b3d2e6b757804805de230821aec2066677d01427b46110a93a8b4725b5b49'
